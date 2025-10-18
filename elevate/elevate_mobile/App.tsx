856998feb6ef76a2536a637e7499e847d26e4bcf
Tanmai Raghava <tanmairaghav3836@gmail.com>
2025-10-19 02:26:55 +0530

    nothing

---
+++ elevate/elevate_mobile/App.tsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <AppNavigator />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
